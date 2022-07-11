04ecfd63cd52dc4bf761739c2cc4bc4e93665a3c
Mike Fulton <mikefultonpersonal@gmail.com>
2022-07-10 21:39:25 -0400

    see if 'wish' really required

--- setenv.sh
+++ setenv.sh
@@ -12,7 +12,7 @@ export PORT_ROOT="${PWD}"
 export PORT_TYPE="GIT"
 
 export PORT_GIT_URL="https://github.com/git/git"
-export PORT_GIT_DEPS="git make m4 perl autoconf automake help2man makeinfo xz zlib openssl curl expat wish gettext"
+export PORT_GIT_DEPS="git make m4 perl autoconf automake help2man makeinfo xz zlib openssl curl expat gettext"
 
 export PORT_EXTRA_CFLAGS=""
 export PORT_EXTRA_LDFLAGS=""
