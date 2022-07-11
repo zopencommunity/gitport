247f17423792801b510474e10b2edda4463f00c0
Mike Fulton <mikefultonpersonal@gmail.com>
2022-07-11 00:46:39 -0400

    builds a number of parts before failing in help.o

--- setenv.sh
+++ setenv.sh
@@ -14,5 +14,8 @@ export PORT_TYPE="GIT"
 export PORT_GIT_URL="https://github.com/git/git"
 export PORT_GIT_DEPS="git make m4 perl autoconf automake help2man makeinfo xz zlib openssl curl expat gettext"
 
-export PORT_EXTRA_CFLAGS=""
-export PORT_EXTRA_LDFLAGS=""
+export PORT_EXTRA_CPPFLAGS="-I\${OPENSSL_HOME}/include,\${GETTEXT_HOME}/include -DNO_REGEX=NeedsStartEnd"
+export PORT_EXTRA_LDFLAGS="-L\${GETTEXT_HOME}/lib -L\${OPENSSL_HOME}/lib"
+
+export PORT_BOOTSTRAP="make"
+export PORT_BOOTSTRAP_OPTS="configure"
