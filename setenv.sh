a0f584d26c79345cc2f71a9d3559a240c827db0e
Mike Fulton <mikefultonpersonal@gmail.com>
2022-07-13 18:25:39 -0400

    patches to get git to build

--- setenv.sh
+++ setenv.sh
@@ -14,8 +14,10 @@ export PORT_TYPE="GIT"
 export PORT_GIT_URL="https://github.com/git/git"
 export PORT_GIT_DEPS="git make m4 perl autoconf automake help2man makeinfo xz zlib openssl curl expat gettext"
 
-export PORT_EXTRA_CPPFLAGS="-I\${OPENSSL_HOME}/include,\${GETTEXT_HOME}/include -DNO_REGEX=NeedsStartEnd"
-export PORT_EXTRA_LDFLAGS="-L\${GETTEXT_HOME}/lib -L\${OPENSSL_HOME}/lib"
+export ZLIB_HOME=$HOME/zot/prod/zlib # msf - temporary
+
+export PORT_EXTRA_CPPFLAGS="-qnocsect -qnose -I\${OPENSSL_HOME}/include,\${GETTEXT_HOME}/include,\${ZLIB_HOME}/include,/usr/include/le -DNO_REGEX=NeedsStartEnd"
+export PORT_EXTRA_LDFLAGS="-L\${GETTEXT_HOME}/lib -L\${OPENSSL_HOME}/lib -L\${ZLIB_HOME}/lib"
 
 export PORT_BOOTSTRAP="make"
 export PORT_BOOTSTRAP_OPTS="configure"
