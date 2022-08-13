0bdcbbfb35208fffa7e7ae44d532c2d3a23ea6bd
Mike Fulton <mikefultonpersonal@gmail.com>
2022-08-12 20:40:29 -0400

    add starter cicd.groovy

---
+++ cicd.groovy
@@ -0,0 +1,15 @@
+node('linux') 
+{
+        stage ('Poll') {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/gitport.git']]])
+        }
+
+        stage('Build') {
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gitport.git'), string(name: 'PORT_DESCRIPTION', value: 'git description' )]
+        }
+}
