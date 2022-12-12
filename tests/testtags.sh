4524a7bef5a1f22acb0d2e98bb0baf47e1a24bb7
Igor Todorovski <itodorov@ca.ibm.com>
2022-12-12 16:36:10 -0500

    Add a test case

---
+++ tests/testtags.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+mydir=$(cd $(dirname $0) && echo $PWD)
+
+
+# The following is a bit hokey... might not always work
+#
+gitdir="${mydir}/../git-2.38.1"
+echo $gitdir
+if ! [ -d "${gitdir}" ] ; then
+  echo "Unable to find git dev driver" >&2
+  exit 99
+fi
+absgitdir=$(cd ${gitdir} && echo $PWD)
+
+export PATH=/bin:/usr/bin:${absgitdir}
+
+tmpdir="/tmp/git-$$"
+mkdir "${tmpdir}" || exit 99             
+cd "${tmpdir}" || exit 99
+touch ".gitconfig" || exit 99
+echo $PWD
+
+export GIT_TEMPLATE_DIR="${mydir}/../git-2.38.1/templates/blt"
+export GIT_EXEC_PATH="${mydir}/../git-2.38.1/libexec/git-core"
+gitrepourl="git@github.com:IgorTodorovskiIBM/EBCDICProject.git"
+git clone $gitrepourl
+
+cat > expected.txt <<ZZ
+t IBM-1047    T=on  README.md
+b binary      T=off a.png
+t ISO8859-1   T=on  ascii.txt
+ZZ
+cd EBCDICProject
+chtag -p * > ../actual.txt
+cd ..
+diff actual.txt expected.txt
